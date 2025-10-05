cea82ed5ad66fd7d2f9627fa895eb33a299f3779
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-10-04 22:38:53 -0300

    Configurando expo router e nativewind

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Olá mundo!</Text>
+      <Text className="text-purple-base">Olá mundo!</Text>
+    </View>
+  );
+}
