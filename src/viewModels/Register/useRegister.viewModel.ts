fc93d146a6296be5c028ca4e0f3a1ae83ee0b2a7
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-18 13:37:53 -0300

    Configurando zustand

--- src/viewModels/Register/useRegister.viewModel.ts
+++ src/viewModels/Register/useRegister.viewModel.ts
@@ -1,10 +1,12 @@
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
-import { RegisterFormData, registerScheme } from "./register.scheme";
-import { useRegisterMutation } from "../../shared/queries/auth/use-register.mutation";
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useForm } from 'react-hook-form'
+import { useRegisterMutation } from '../../shared/queries/auth/use-register.mutation'
+import { useUserStore } from '../../shared/store/user-store'
+import { RegisterFormData, registerScheme } from './register.scheme'
 
 export const useRegisterViewModel = () => {
-  const userRegisterMutation = useRegisterMutation();
+  const userRegisterMutation = useRegisterMutation()
+  const { setSession, user } = useUserStore()
 
   const {
     control,
@@ -13,24 +15,33 @@ export const useRegisterViewModel = () => {
   } = useForm<RegisterFormData>({
     resolver: yupResolver(registerScheme),
     defaultValues: {
-      name: "teste",
-      email: "teste@gmail.com",
-      password: "123123123",
-      confirmPassword: "123123123",
-      phone: "11111111111",
+      name: 'teste2',
+      email: 'teste2@gmail.com',
+      password: '123123123',
+      confirmPassword: '123123123',
+      phone: '11111111111',
     },
-  });
+  })
 
   const onSubmit = handleSubmit(async (userData) => {
-    console.log(userData);
-    const { confirmPassword, ...registerData } = userData;
+    console.log(userData)
+    const { confirmPassword, ...registerData } = userData
 
-    await userRegisterMutation.mutateAsync(registerData);
-  });
+    const mutationResponse =
+      await userRegisterMutation.mutateAsync(registerData)
+
+    setSession({
+      refreshToken: mutationResponse.refreshToken,
+      token: mutationResponse.token,
+      user: mutationResponse.user,
+    })
+  })
+
+  console.log(user)
 
   return {
     control,
     errors,
     onSubmit,
-  };
-};
+  }
+}
