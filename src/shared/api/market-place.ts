3fb4d8a8e680b37ff2df867244afde3dfcabb0f1
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-21 16:05:38 -0300

    Adicionando Bearer Token nas requisições

--- src/shared/api/market-place.ts
+++ src/shared/api/market-place.ts
@@ -1,29 +1,56 @@
-import axios, { AxiosInstance } from "axios";
-
-import { Platform } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import axios, { AxiosInstance } from 'axios'
+import { Platform } from 'react-native'
 
 const getBaseURL = () => {
   return Platform.select({
-    ios: "http://localhost:3001",
-    android: "http://10.0.2.2:3001",
-  });
-};
+    ios: 'http://localhost:3001',
+    android: 'http://10.0.2.2:3001',
+  })
+}
 
-export const baseURL = getBaseURL();
+export const baseURL = getBaseURL()
 
 export class MarketPlaceApiClient {
-  private instance: AxiosInstance;
-  private isRefreshing = false;
+  private instance: AxiosInstance
+  private isRefreshing = false
 
   constructor() {
     this.instance = axios.create({
       baseURL,
-    });
+    })
+
+    this.setupInterceptors()
   }
 
   getInstance() {
-    return this.instance;
+    return this.instance
+  }
+
+  private setupInterceptors() {
+    this.instance.interceptors.request.use(
+      async (config) => {
+        const userData = await AsyncStorage.getItem('marketplace-auth')
+        console.log(userData)
+        if (userData) {
+          const {
+            state: { token },
+          } = JSON.parse(userData)
+
+          console.log(token)
+
+          if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+          }
+        }
+
+        return config
+      },
+      (error) => {
+        return Promise.reject(error)
+      },
+    )
   }
 }
 
-export const marketPlaceApiClient = new MarketPlaceApiClient().getInstance();
+export const marketPlaceApiClient = new MarketPlaceApiClient().getInstance()
