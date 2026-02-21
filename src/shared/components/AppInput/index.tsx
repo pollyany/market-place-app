f55dd0c4a52fdf925231ba8b188801517ffcb3c9
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-21 09:18:14 -0300

    Criando ViewModel do input global

--- src/shared/components/AppInput/index.tsx
+++ src/shared/components/AppInput/index.tsx
@@ -1,19 +1,70 @@
 import { Ionicons } from '@expo/vector-icons'
-import { Pressable, TextInput, TouchableOpacity, View } from 'react-native'
-import { appInputVariants } from './input.variants'
+import { FC } from 'react'
+import {
+  Pressable,
+  Text,
+  TextInput,
+  TextInputProps,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import { AppInputVariantsProps, appInputVariants } from './input.variants'
+import { useAppInputViewModel } from './useAppInputViewModel'
 
-export const AppInput = () => {
+export interface AppInputProps extends TextInputProps, AppInputVariantsProps {
+  label?: string
+  leftIcon?: keyof typeof Ionicons.glyphMap
+  rightIcon?: keyof typeof Ionicons.glyphMap
+  containerClassName?: string
+  mask?: (value: string) => void | string
+  error?: string
+}
+
+export const AppInput: FC<AppInputProps> = ({
+  label,
+  leftIcon,
+  rightIcon,
+  containerClassName,
+  mask,
+  value,
+  isError,
+  secureTextEntry,
+  onBlur,
+  onFocus,
+  onChangeText,
+  error,
+  isDisabled,
+  ...textInputProps
+}) => {
+  const {
+    getIconColor,
+    handleWrapperPress,
+    handlePasswordToggle,
+    handleFocus,
+    handleBlur,
+  } = useAppInputViewModel({
+    error,
+    onBlur,
+    onFocus,
+    isError: !!error,
+    mask,
+    onChangeText,
+    isDisabled,
+    secureTextEntry,
+    value,
+  })
   const styles = appInputVariants()
 
   return (
-    <View>
-      <Pressable>
-        <Ionicons name="person" />
+    <View className={styles.container({ className: containerClassName })}>
+      <Text className={styles.label()}>Label</Text>
+      <Pressable className={styles.wrapper()}>
+        <Ionicons size={22} name="person" />
 
-        <TextInput />
+        <TextInput className={styles.input()} {...textInputProps} />
 
         <TouchableOpacity>
-          <Ionicons name="eye-off-outline" />
+          <Ionicons size={22} name="eye-off-outline" />
         </TouchableOpacity>
       </Pressable>
     </View>
