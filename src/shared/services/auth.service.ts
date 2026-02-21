15d36c4863acead5401b23c8fcfae0d112685737
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-21 11:05:36 -0300

    Criando mutation de login

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -1,12 +1,22 @@
 import { marketPlaceApiClient } from '../api/market-place'
+import { AuthResponse } from '../interfaces/http/auth-response'
+import { LoginHttpParams } from '../interfaces/http/login'
 import {
   RegisterHttpParams,
-  RegisterHttpResponse,
 } from '../interfaces/http/register'
 
 export const register = async (userData: RegisterHttpParams) => {
-  const { data } = await marketPlaceApiClient.post<RegisterHttpResponse>(
+  const { data } = await marketPlaceApiClient.post<AuthResponse>(
     '/auth/register',
+    userData,
+  )
+
+  return data
+}
+
+export const login = async (userData: LoginHttpParams) => {
+  const { data } = await marketPlaceApiClient.post<AuthResponse>(
+    '/auth/login',
     userData,
   )
 
