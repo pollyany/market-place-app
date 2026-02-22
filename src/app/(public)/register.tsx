1f448497820c8e9d7f23cfca62eae2549da429a3
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-22 14:39:03 -0300

    Orquestrando rotas públicas e privadas

---
+++ src/app/(public)/register.tsx
@@ -0,0 +1,8 @@
+import { useRegisterViewModel } from "../../viewModels/Register/useRegister.viewModel";
+import { RegisterView } from "../../viewModels/Register/Register.view";
+
+export default function Register() {
+  const props = useRegisterViewModel();
+
+  return <RegisterView {...props} />;
+}
--- src/app/register.tsx
+++
@@ -1,8 +0,0 @@
-import { useRegisterViewModel } from "../viewModels/Register/useRegister.viewModel";
-import { RegisterView } from "../viewModels/Register/Register.view";
-
-export default function Register() {
-   const props = useRegisterViewModel()
-
-  return <RegisterView {...props} />
-}
