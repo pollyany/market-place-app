dbf5a9f8baa218deda236ed529a522517680b853
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-22 14:44:03 -0300

    Desautenticando usuário caso os tokens estejam válidos

--- src/shared/api/market-place.ts
+++ src/shared/api/market-place.ts
@@ -1,6 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import axios, { AxiosInstance } from 'axios'
 import { Platform } from 'react-native'
+import { useUserStore } from '../store/user-store'
 
 const getBaseURL = () => {
   return Platform.select({
@@ -52,6 +53,7 @@ export class MarketPlaceApiClient {
     this.instance.interceptors.response.use(
       (response) => response,
       async (error) => {
+        alert('CAIU NO REFRESH')
         const originalRequest = error.config
 
         if (
@@ -94,6 +96,7 @@ export class MarketPlaceApiClient {
 
             return axios(originalRequest)
           } catch {
+            this.handleUnauthorized()
             return Promise.reject(
               new Error('Sessão expirada, faça o login novamente.'),
             )
@@ -109,6 +112,13 @@ export class MarketPlaceApiClient {
         }
       },
     )
+  }
+
+  private async handleUnauthorized() {
+    const { logout } = useUserStore.getState()
+
+    delete this.instance.defaults.headers.common.Authorization
+    logout()
   }
 }
 
