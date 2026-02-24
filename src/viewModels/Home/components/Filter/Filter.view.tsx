441550a5f6676f02692fd6ba7ca958e1c17f46fa
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-24 11:55:19 -0300

    Consumindo rota de busca de categorias

--- src/viewModels/Home/components/Filter/Filter.view.tsx
+++ src/viewModels/Home/components/Filter/Filter.view.tsx
@@ -1,7 +1,57 @@
+import { Ionicons } from '@expo/vector-icons'
 import { FC } from 'react'
-import { View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { AppButton } from '../../../../shared/components/AppButton'
+import { AppInput } from '../../../../shared/components/AppInput'
+import { colors } from '../../../../styles/colors'
 import { useFilterViewModel } from './useFilter.viewModel'
 
-export const FilterView: FC<ReturnType<typeof useFilterViewModel>> = () => {
-  return <View></View>
+export const FilterView: FC<ReturnType<typeof useFilterViewModel>> = ({
+  productCategories,
+  isLoading,
+}) => {
+
+  return (
+    <View>
+      <View className="flex-row items-center justify-between py-4 px-6">
+        <Text className="text-lg font-bold text-gray-900">
+          Filtrar anúncios
+        </Text>
+        <TouchableOpacity>
+          <Ionicons name="close" size={20} color={colors['purple-base']} />
+        </TouchableOpacity>
+      </View>
+
+      <View className="py-4 px-6">
+        <Text className="font-semibold text-base text-gray-300">VALOR</Text>
+        <View className="flex-row mb-4 w-full">
+          <View className="flex-1">
+            <AppInput
+              placeholder="De"
+              keyboardType="numeric"
+              containerClassName="w-[90%]"
+            />
+          </View>
+          <View className="flex-1">
+            <AppInput
+              placeholder="Até"
+              keyboardType="numeric"
+              containerClassName="w-[90%]"
+            />
+          </View>
+        </View>
+
+        <Text className="font-semibold text-base text-gray-300">CATEGORIA</Text>
+
+        <View className="flex-row gap-3 mt-4 mb-6">
+          <View className="flex-1">
+            <AppButton variant="outlined">Limpar filtro</AppButton>
+          </View>
+          <View className="flex-1">
+            <AppButton>Filtrar</AppButton>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
 }
