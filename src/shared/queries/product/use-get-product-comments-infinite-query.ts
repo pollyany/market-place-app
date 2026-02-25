12a8fcc9e041ee38b09d062f888eb69463d776cb
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-02-25 11:03:10 -0300

    Ajustando BottomSheet de comentário

--- src/shared/queries/product/use-get-product-comments-infinite-query.ts
+++ src/shared/queries/product/use-get-product-comments-infinite-query.ts
@@ -1,7 +1,8 @@
-import { useInfiniteQuery } from '@tanstack/react-query'
-import { buildImageUrl } from '../../helpers/buildImageUrl'
-import { getProductComments } from '../../services/product.service'
-import { ProductComment } from '../../interfaces/product-comment'
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { buildImageUrl } from "../../helpers/buildImageUrl";
+import { getProductComments } from "../../services/product.service";
+import { ProductComment } from "../../interfaces/product-comment";
+import { baseURL } from "../../api/market-place";
 
 export const useGetProductCommentsInfiniteQuery = (productId: number) => {
   const query = useInfiniteQuery({
@@ -10,31 +11,33 @@ export const useGetProductCommentsInfiniteQuery = (productId: number) => {
         productId,
         pagination: { page: pageParam, perPage: 20 },
       }),
-    queryKey: ['product-comments', productId],
+    queryKey: ["product-comments", productId],
     getNextPageParam: (lastPage) => {
       if (lastPage.page < lastPage.totalPages) {
-        return lastPage.page + 1
+        return lastPage.page + 1;
       }
-      return undefined
+      return undefined;
     },
     initialPageParam: 1,
-  })
+  });
 
   const comments =
-    query.data?.pages
+    (query.data?.pages
       .flatMap((page) => page.data)
       .map((comment) => ({
         ...comment,
         user: {
           ...comment.user,
           avatar: {
-            url: buildImageUrl(comment.user.avatar?.url ?? '') ?? '',
+            url: comment.user.avatar.url
+              ? `${baseURL}${comment.user.avatar.url}`
+              : undefined,
           },
         },
-      })) as unknown as ProductComment[] ?? []
+      })) as unknown as ProductComment[]) ?? [];
 
   return {
     ...query,
     comments,
-  }
-}
+  };
+};
