98b1288aec1dd29ce2e45f18297d40e6ab07bd02
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-03-02 08:01:14 -0300

    Header de pedidos

--- src/viewModels/Orders/Orders.view.tsx
+++ src/viewModels/Orders/Orders.view.tsx
@@ -4,18 +4,20 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { OrderItem } from './components/OrderItem'
 import { useOrdersViewModel } from './useOrders.viewModel'
 import { EmptyList } from './components/EmptyList'
+import { ListHeader } from './components/ListHeader'
 
 export const OrdersView: FC<ReturnType<typeof useOrdersViewModel>> = ({
   orders,
 }) => {
   return (
-    <SafeAreaView className="flex-1">
+    <SafeAreaView edges={['top']} className="flex-1">
       <FlatList
         contentContainerClassName="px-[16px] pb-[120px]"
         data={orders}
         renderItem={({ item: order }) => <OrderItem order={order} />}
         keyExtractor={({ id }) => `order-${id}`}
-        ListEmptyComponent={<EmptyList />}
+        ListEmptyComponent={EmptyList}
+        ListHeaderComponent={ListHeader}
       />
     </SafeAreaView>
   )
