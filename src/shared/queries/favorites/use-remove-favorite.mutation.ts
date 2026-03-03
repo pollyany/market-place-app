35091f7fec234eb6ef942431b5341e4c0052837f
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-03-03 15:40:33 -0300

    Adicionando e removendo produto dos favoritos

--- src/shared/queries/favorites/use-remove-favorite.mutation.ts
+++ src/shared/queries/favorites/use-remove-favorite.mutation.ts
@@ -1,19 +1,19 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { Toast } from 'toastify-react-native'
-import { removeFavorite } from '../../services/favorites.service'
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Toast } from "toastify-react-native";
+import { removeFavorite } from "../../services/favorites.service";
 
 export const useRemoveFavoriteMutation = () => {
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: (productId: number) => removeFavorite(productId),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['favorites'] })
-      Toast.success('Produto removido dos favoritos')
+      queryClient.invalidateQueries({ queryKey: ["favorites"] });
+      Toast.success("Produto removido dos favoritos", "bottom");
     },
     onError: () => {
-      Toast.error('Falha ao remover produto dos favoritos')
+      Toast.error("Falha ao remover produto dos favoritos", "bottom");
     },
-  })
+  });
 
-  return mutation
-}
+  return mutation;
+};
