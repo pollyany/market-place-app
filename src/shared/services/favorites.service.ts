de4af7d19b03b780ba6f7b51b2931e5b33e68568
Pollyany Carvalho <kerleypollyany@gmail.com>
2026-03-03 15:36:00 -0300

    Criando queries e mutations de favorites

--- src/shared/services/favorites.service.ts
+++ src/shared/services/favorites.service.ts
@@ -1,15 +1,19 @@
-import { marketPlaceApiClient } from '../api/market-place'
-import { Favorite } from '../interfaces/http/favorite'
+import { marketPlaceApiClient } from "../api/market-place";
+import { AddFavoriteResponse, Favorite } from "../interfaces/http/favorite";
 
 export const getFavorites = async (): Promise<Favorite[]> => {
-  const { data } = await marketPlaceApiClient.get<Favorite[]>('/favorites')
-  return data
-}
+  const { data } = await marketPlaceApiClient.get<Favorite[]>("/favorites");
+  return data;
+};
 
 export const addFavorite = async (productId: number) => {
-  await marketPlaceApiClient.post('/favorites', { productId })
-}
+  const { data } = await marketPlaceApiClient.post<AddFavoriteResponse>(
+    "/favorites",
+    { productId },
+  );
+  return data;
+};
 
 export const removeFavorite = async (productId: number) => {
-  await marketPlaceApiClient.delete(`/favorites/${productId}`)
-}
+  await marketPlaceApiClient.delete(`/favorites/${productId}`);
+};
